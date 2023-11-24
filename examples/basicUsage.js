0626fa3ca4ac566398ea67ffaad1bbde00ecaccb
EnzoVezzaro <enzo-vezzaro@live.it>
2023-11-24 15:57:20 -0400

    add DGII examples

--- examples/basicUsage.js
+++ examples/basicUsage.js
@@ -20,7 +20,7 @@ console.log('Authentication Result:', authenticationResult);
 // Full documentation: https://developer.digital.gob.do/apis/ff9ce928-e16e-4ea9-9ce9-28e16e1ea96e
 async function getCitizenData() {
   try {
-    const isValid = await aletheiaSDK.services.validateCitizenCedula(4025357591);
+    const isValid = await aletheiaSDK.services.validateCitizenCedula('XXX');
     console.log('Citizen Cedula:', isValid);
   } catch (error) {
     console.error('Error retrieving citizen data:', error);
@@ -33,7 +33,7 @@ async function getCitizenData() {
 // Full documentation: https://developer.digital.gob.do/apis/fdf3319d-521e-4364-b331-9d521e636442
 async function getFuelPrices() {
   try {
-    const fuelPrices = await aletheiaSDK.opendata.fuelPrices('date', '2021-12-12');
+    const fuelPrices = await aletheiaSDK.opendata.fuelPrices('date', '2023-11-01');
     console.log('Fuel Prices:', fuelPrices);
   } catch (error) {
     console.error('Error retrieving fuel prices:', error);
@@ -92,13 +92,90 @@ async function getDGCPData() {
 
 // Example: Retrieve DGII data for the Dominican Republic
 // Full documentation: https://dgii.gov.do/wsMovilDGII/WSMovilDGII.asmx
-async function getDGIIData() {
+const dgiiService = aletheiaSDK.opendata.gob('dgii');
+// test numbers
+const rncContribuyente = '130102058';
+const ncfContribuyente = 'B0100000003';
+async function getDGIIContribuyentes() {
     try {
-        const dgiiData = await aletheiaSDK.opendata.gob('dgii').getContribuyentes();
-        console.log('DGII Data:', dgiiData);
+        const result = await dgiiService.getContribuyentes(rncContribuyente);
+        console.log('GetContribuyentes Result:', result);
     } catch (error) {
-        console.error('Error retrieving DGII data:', error);
+        console.error('Error retrieving GetContribuyentes data:', error);
     }
 }
 
-getDGIIData();
+async function getDGIIContribuyentesCount() {
+    try {
+        const result = await dgiiService.getContribuyentesCount(rncContribuyente);
+        console.log('GetContribuyentesCount Result:', result);
+    } catch (error) {
+        console.error('Error retrieving GetContribuyentesCount data:', error);
+    }
+}
+
+async function getDGIIDocumento() {
+    try {
+        const codigoBusqueda = 'ABC123';
+        const patronBusquedaDocumento = 1;
+        const imeiDocumento = 'your_imei_value';
+        const result = await dgiiService.getDocumento(codigoBusqueda, patronBusquedaDocumento, imeiDocumento);
+        console.log('GetDocumento Result:', result);
+    } catch (error) {
+        console.error('Error retrieving GetDocumento data:', error);
+    }
+}
+
+async function getDGIIVehiculoPorDATAMATRIX() {
+    try {
+        const valueVehiculo = 'DATAMATRIX123';
+        const imeiVehiculo = 'your_imei_value';
+        const result = await dgiiService.getVehiculoPorDATAMATRIX(valueVehiculo, imeiVehiculo);
+        console.log('GetVehiculoPorDATAMATRIX Result:', result);
+    } catch (error) {
+        console.error('Error retrieving GetVehiculoPorDATAMATRIX data:', error);
+    }
+}
+
+async function getDGIINCF() {
+    try {
+        const imeiNCF = 'your_imei_value';
+        const result = await dgiiService.getNCF(rncContribuyente, ncfContribuyente, imeiNCF);
+        console.log('GetNCF Result:', result);
+    } catch (error) {
+        console.error('Error retrieving GetNCF data:', error);
+    }
+}
+
+async function getDGIINCF2() {
+    try {
+        const rncComprador = '130102058';
+        const codSeguridad = '789012';
+        const imeiNCF2 = 'your_imei_value';
+        const result = await dgiiService.getNCF2(rncContribuyente, ncfContribuyente, rncComprador, codSeguridad, imeiNCF2);
+        console.log('GetNCF2 Result:', result);
+    } catch (error) {
+        console.error('Error retrieving GetNCF2 data:', error);
+    }
+}
+
+async function getSolicitud() {
+    try {
+        const solicitud = 'SOLICITUD123';
+        const IMEI = 'your_imei_value';
+        const dgiiData = await aletheiaSDK.opendata.gob('dgii').getSolicitud(rncContribuyente, solicitud, IMEI);
+        console.log('Solicitud Data:', dgiiData);
+    } catch (error) {
+        console.error('Error retrieving Solicitud data:', error);
+    }
+}
+
+// Call the methods
+getDGIIContribuyentes();
+// getDGIIContribuyentesCount();
+// getDGIIDocumento();
+// getDGIIVehiculoPorDATAMATRIX();
+// getDGIINCF();
+// getDGIINCF2();
+// getSolicitud();
+
